8485e198b400da573f39e88884d3de0d938594f6
Pavlo Ovchynnykov <gusovchpa@student.gu.se>
2025-01-28 14:33:28 +0100

    add exercises 2 and 3

---
+++ wp1/ex2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h> // The I/O header file contains the necessary logic for printing and inputting information
+#include <string.h> // The string header file containts the necessary methods to interact with strings.
+#include <ctype.h> // The ctype header file is used to identify the the type of the inputs aswell as changing the parameter to another data type.
+
+#define ALPHABET_SIZE 26 // Constant used as a divisor for the ASCII values for the alphabet.
+#define BASE_10 10 // Constant used as a divisor for the ASCII values of the base 10
+
+int main(int argc, char **argv) {
+    char userInput[26]; // Where the message from the user will be stored to be encrypted later on.
+
+    while (1) { // As long as something inside of the loop does not exit, keep going.
+        printf("Please provide the message that will be encrypted: \n"); // Output to inform the user to provide their message.
+         
+        if (!fgets(userInput, sizeof(userInput), stdin)){ // If the input from the user is not a message (EOF), then exit the program.
+            printf("End of line."); // Inform the user that the program ended with "End of line." message
+            break; // Exit the loop and end the program.
+        }
+        
+        printf("The provided input: %s\n", userInput); // Shows the input provided by the user.
+        
+        for (int i = 0; i < strlen(userInput); i++) { // Iterate for each character of the message provided by the user.
+            if (isalpha(userInput[i])) { // Checks if the current character in the message is a letter.
+                if (isupper(userInput[i])) { // Checks if the current letter is of uppercase
+                    userInput[i] = 'A' + (userInput[i] - 'A' + atoi(argv[1])) % ALPHABET_SIZE; // Use the ASCII values aswell as the parameter to determine the encrypted uppercase letter.
+                } else { // If the letter is not uppercase, then it must be lowercase.
+                    userInput[i] = 'a' + (userInput[i] - 'a' + atoi(argv[1])) % ALPHABET_SIZE; // Use the ASCII values aswell as the parameter to determine the encrypted lowercase letter.
+                }
+            } else if (isdigit(userInput[i])) { // Checks if the current character is a number.
+                userInput[i] = '0' + (userInput[i] - '0' + atoi(argv[1])) % BASE_10; // Use the ASCII values aswell as the parameter to determine the encrypted number.
+            }
+        }
+        
+        printf("Encrypted: %s\n", userInput); // Show the user the encrypted version of their input.
+    }
+    
+    return 0; // Indicates that the system has run with no issues indicated.
+}
---
+++ wp1/ex3/main.c
@@ -0,0 +1,88 @@
+#include <stdio.h> // The I/O header file contains the necessary logic for printing and inputting information
+#include <stdlib.h>
+#include <string.h> // The string header file containts the necessary methods to interact with strings.
+#include <ctype.h> // The ctype header file is used to identify the the type of the inputs aswell as changing the parameter to another data type.
+#include <time.h>  // Time header used to get a seed for RNG
+
+#define MAX_NUM_GUESSES 10  // max number of guesses given to user
+#define MAX_INPUT_NUM 100   // upper limit of generated number
+#define MIN_INPUT_NUM 1     // lower limit of generated number
+
+#define bool int            // macro for boolean
+#define true 1              // macro for true 
+#define false 0             // macro for false 
+
+int randNumMinMax(int min, int max);
+
+
+// play a round of a guessing game with user 
+void play_round() {
+    int user_guesses = 0;                               // user guesses counter
+    int randomNumber = randNumMinMax(MIN_INPUT_NUM, MAX_INPUT_NUM);// generate a number for the game  
+    while (user_guesses < MAX_NUM_GUESSES) {// play while the guess counter hasn't exceeded the limit 
+        user_guesses++;                                 // increment the guesses number at the start of the round
+
+        int isNumber = 1;
+        printf("Put in your guess: ");
+        fgets(userInput, MAX_INPUT_NUM, stdin);
+        
+        if (userInput[strlen(userInput) - 1] == '\n') {
+            userInput[strlen(userInput) - 1] = '\0';
+        }
+
+        for (int i = 0; i < strlen(userInput); i++) {
+            if (!isdigit((int)userInput[i])) {
+                isNumber = 0;
+                break;
+            }
+        }
+
+        if (!isNumber) {
+            printf("The input should only contain a number.\n");
+            continue;
+        }
+
+        int guess = atoi(userInput);
+
+        if (guess < MIN_INPUT_NUM || guess > MAX_INPUT_NUM) {
+            printf("The number provided was not within the 1-100 threshold.\n");
+            continue;
+        }
+
+        if (guess == randomNumber) {                    // if user guesses correctly... 
+            printf("You have guessed %d times and your guess is correct!\n", user_guesses); // print victory message & 
+            return;                                     // exit function
+        } else if(guess < randomNumber) {               // else if the guess is too low ...
+            printf("Your guess is too low, please try again\n"); // print that the number is bigger
+        } else if (guess > randomNumber) {              // else if the guess is too high ... 
+            printf("Your guess is too high, please try again\n");// print that the number is smaller 
+        }
+    }
+
+    printf("You have exceeded the maximum number of guesses"); // print loss message
+    return;                                             // exit function
+
+}
+
+int main(int argc, char **argv) {
+
+    srand(time(0));                                     // set a seed for RNG based on time
+    
+    bool play_again = false;                                // bool to check if user wants to play again
+    do {                                                    // start the game 
+        play_round();
+        
+        printf("Play again? 1-yes, 0-no: ");                // prompt player if they want to play again 
+        scanf("%d", &play_again);                            // save players decisicion
+        
+    } while (play_again);                                   // loop if player decides to play again
+    
+
+    return 0;                                               // exit the program
+}
+
+// get a random number in range min..max 
+int randNumMinMax(int min, int max) {
+    return rand() % (max - min + 1) + min;                  // generate a random number in range min..max
+    
+}
