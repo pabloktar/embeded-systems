cb03a2530c25b6b81b24f245c285d1762d029ab8
Pavlo Ovchynnykov <gusovchpa@student.gu.se>
2025-01-28 16:42:10 +0100

    add comments to exrecise3

--- wp1/ex3/main.c
+++ wp1/ex3/main.c
@@ -2,7 +2,7 @@
 // Work package 1
 // Exercise 3
 // Submission code: 452706
-#include <stdio.h> // The I/O header file contains the necessary logic for printing and inputting information
+// #include <stdio.h> // The I/O header file contains the necessary logic for printing and inputting information
 #include <stdlib.h>
 #include <string.h> // The string header file containts the necessary methods to interact with strings.
 #include <ctype.h> // The ctype header file is used to identify the the type of the inputs aswell as changing the parameter to another data type.
@@ -11,6 +11,7 @@
 #define MAX_NUM_GUESSES 10  // max number of guesses given to user
 #define MAX_INPUT_NUM 100   // upper limit of generated number
 #define MIN_INPUT_NUM 1     // lower limit of generated number
+#define MAX_INPUT_LEN 100   
 
 #define bool int            // macro for boolean
 #define true 1              // macro for true 
@@ -23,35 +24,37 @@ int randNumMinMax(int min, int max);
 void play_round() {
     int user_guesses = 0;                               // user guesses counter
     int randomNumber = randNumMinMax(MIN_INPUT_NUM, MAX_INPUT_NUM);// generate a number for the game  
+    char userInput[MAX_INPUT_LEN];                                 // Array to store the user's input
     while (user_guesses < MAX_NUM_GUESSES) {// play while the guess counter hasn't exceeded the limit 
-        user_guesses++;                                 // increment the guesses number at the start of the round
 
-        int isNumber = 1;
-        printf("Put in your guess: ");
-        fgets(userInput, MAX_INPUT_NUM, stdin);
+        int isNumber = 1;                   // Flag to check if the input is a valid number
+        printf("Put in your guess: ");      // Print message to user to put i ntheir guess
+        fgets(userInput, MAX_INPUT_NUM, stdin); // Get the user's input
         
-        if (userInput[strlen(userInput) - 1] == '\n') {
-            userInput[strlen(userInput) - 1] = '\0';
+        if (userInput[strlen(userInput) - 1] == '\n') { // Check if the last character of the input is a newline
+            userInput[strlen(userInput) - 1] = '\0';   // Replace the newline character with a null terminator
         }
 
-        for (int i = 0; i < strlen(userInput); i++) {
-            if (!isdigit((int)userInput[i])) {
-                isNumber = 0;
-                break;
+        for (int i = 0; i < strlen(userInput); i++) {           // Loop to check if each character is an integer
+            if (!isdigit((int)userInput[i])) {                  // Check if the current character is an integer
+                isNumber = 0;                                   // If current character is not a integer, set isNumber = 0 (act as false in boolean)
+                break;                                          // Break out of loop
             }
         }
 
-        if (!isNumber) {
-            printf("The input should only contain a number.\n");
-            continue;
+        if (!isNumber) {                                        // Check if user input contained a non-integer
+            printf("The input should only contain a number.\n");// Print warning message if user input is not an integer
+            continue;                                           // Skip the loop iteration
         }
 
         int guess = atoi(userInput);
 
-        if (guess < MIN_INPUT_NUM || guess > MAX_INPUT_NUM) {
-            printf("The number provided was not within the 1-100 threshold.\n");
-            continue;
+        if (guess < MIN_INPUT_NUM || guess > MAX_INPUT_NUM) {   // Checker for if guess is within threshold
+            printf("The number provided was not within the 1-100 threshold.\n");  // Print warning message if message user input is not within given range
+            continue;                                           // Skip the loop iteration
         }
+
+        user_guesses++;                         // increment the guesses number at the start of the round
 
         if (guess == randomNumber) {                    // if user guesses correctly... 
             printf("You have guessed %d times and your guess is correct!\n", user_guesses); // print victory message & 
@@ -68,7 +71,7 @@ void play_round() {
 
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) {                        
 
     srand(time(0));                                     // set a seed for RNG based on time
     
@@ -77,16 +80,14 @@ int main(int argc, char **argv) {
         play_round();
         
         printf("Play again? 1-yes, 0-no: ");                // prompt player if they want to play again 
-        scanf("%d", &play_again);                            // save players decisicion
+        scanf("%d", &play_again);                           // save players decisicion
+        while (getchar() != '\n');                          // clear buffer until next new line (we don't have a input checker here)
         
     } while (play_again);                                   // loop if player decides to play again
     
-
     return 0;                                               // exit the program
 }
 
-// get a random number in range min..max 
-int randNumMinMax(int min, int max) {
+int randNumMinMax(int min, int max) {                       // get a random number in range min..max 
     return rand() % (max - min + 1) + min;                  // generate a random number in range min..max
-    
 }
