4356e6ec2ad30650d5229cebaa836d12e44b7225
Pavlo Ovchynnykov <gusovchpa@student.gu.se>
2025-02-11 13:45:11 +0100

    comment wp2/ex4

--- wp2/ex4/code.c
+++ wp2/ex4/code.c
@@ -2,60 +2,65 @@
 #include "stdio.h"
 #include "string.h"
 
-#define byte unsigned char
-#define bit0 0b1
-#define bit1 0b10
-#define bit2 0b100
-#define bit3 0b1000
-#define bit4 0b10000
-#define bit5 0b100000
-#define bit6 0b1000000
-#define bit7 0b10000000
+#define byte unsigned char  // macro for byte type;
+#define bit0 0b1            // |
+#define bit1 0b10           // |
+#define bit2 0b100          // |
+#define bit3 0b1000         // | macros for bits 0 to 7;
+#define bit4 0b10000        // |
+#define bit5 0b100000       // |
+#define bit6 0b1000000      // |
+#define bit7 0b10000000     // |
 
-enum ARGV{ENGINE_ON = 1, GEAR_POS = 2, KEY_POS = 3, BRAKE1 = 4, BRAKE2 = 5};
+enum ARGV{ENGINE_ON = 1, GEAR_POS = 2, KEY_POS = 3, BRAKE1 = 4, BRAKE2 = 5};// enum for data indexes;
 
+
+// Funciton to check the input to be the number in range from 0 to "range";
+// Input format: 
+//  - str: input string;
+//  - range: allowed range of the input;
 int checkInput(char *str, int range){
-    if (strlen(str) > 1){
-        return 0;
+    if (strlen(str) > 1){       // only allow one digit inputs;
+        return 0;               // exit the function with error code;
     }
-    char c = str[0];
-    if (c < '0' || c > '9'){
-        return 0;
+    char c = str[0];            // convert string to char;
+    if (c < '0' || c > '9'){    // only allow digit inputs;
+        return 0;               // exit the function with error code;
     }
-    char num = c - '0';
-    if (num > range){
-        return 0;
+    int num = c - '0';          // convert char to int;
+    if (num > range){           // allow only the nubmers in the 0.."range";
+        return 0;               // exit the funtion with error code;
     }
-    return 1;
+    return 1;                   // exit the function;
 }
 
 int main (int argc, char **argv){
-    if (argc != 6){                 // check if the number of arguments provided is exactly 5;
+    if (argc != 6){                                 // check if the number of arguments provided is exactly 5;
         printf("ERROR: number of arguments provided is not supported.");// print error message;
-        return 1;                   // exit the program with error code 2;
+        return 1;                                   // exit the program with error code 2;
     }
 
-    int ranges[6] = {0, 1, 4, 2, 1, 1};
-    int intArgv[6] = {0, 0, 0, 0, 0, 0};
+    int ranges[6] = {0, 1, 4, 2, 1, 1};             // declare the ranges for encoded values;
+    int intArgv[6] = {0, 0, 0, 0, 0, 0};            // declare array to store argv as ints;
 
-    for (int i = ENGINE_ON; i <= BRAKE2; i++) {
-        if (checkInput(argv[i], ranges[i])) {
-            intArgv[i] = atoi(argv[i]);
+    for (int i = ENGINE_ON; i <= BRAKE2; i++) {     // loop through arguments;
+        if (checkInput(argv[i], ranges[i])) {       // verify the argument;
+            intArgv[i] = atoi(argv[i]);             // convert argv from String to int;
         } else {
-            printf("ERROR: wrong argument format");
-            return 2;
+            printf("ERROR: wrong argument format"); // print error message;
+            return 2;                               // exit program with error code 2;
         }
     }
 
-    byte code = 0;
+    byte code = 0;                                  // initialise code;
     
-    code = code | (bit7 * intArgv[ENGINE_ON]);
-    code = code | (bit4 * intArgv[GEAR_POS]);
-    code = code | (bit2 * intArgv[KEY_POS]);
-    code = code | (bit1 * intArgv[BRAKE1]);
-    code = code | (bit0 * intArgv[BRAKE2]);
+    code = code | (bit7 * intArgv[ENGINE_ON]);      // encode ENGINE_ON;
+    code = code | (bit4 * intArgv[GEAR_POS]);       // encode GEAR_POS;
+    code = code | (bit2 * intArgv[KEY_POS]);        // encode KEY_POS;
+    code = code | (bit1 * intArgv[BRAKE1]);         // encode BRAKE1;
+    code = code | (bit0 * intArgv[BRAKE2]);         // encode BRAKE2;
     
-    printf("%X", code);
+    printf("%X", code);             // print hexadecimal code;
 
     return 0;                       // exit the program
 }
--- wp2/ex4/decode.c
+++ wp2/ex4/decode.c
@@ -1,64 +1,72 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
-#define bit0 0b1
-#define bit1 0b10
-#define bit2 0b100
-#define bit3 0b1000
-#define bit4 0b10000
-#define bit5 32
-#define bit6 64
-#define bit7 128
+#define bit0 0b1        // |
+#define bit1 0b10       // |
+#define bit2 0b100      // |
+#define bit3 0b1000     // | macros for bits from 0 to 7;
+#define bit4 0b10000    // |
+#define bit5 32         // |
+#define bit6 64         // |
+#define bit7 128        // |
 
 
-enum DATA{ENGINE_ON = 0, GEAR_POS = 1, KEY_POS = 2, BRAKE1 = 3, BRAKE2 = 4};
+enum DATA{ENGINE_ON = 0, GEAR_POS = 1, KEY_POS = 2, BRAKE1 = 3, BRAKE2 = 4};// enum for DATA;
 
+
+// Decoder function. Takes the code in and writes the decoded data in the decode array;
 int decoder (int code, char* decode){
-    if(code < 0 | code > 255){
-        return 1;
+    if(code < 0 | code > 255){                              // verify that code fits a byte;
+        return 1;                                           // exit function with error code; 
     }
-    decode[ENGINE_ON] = (code & bit7) / bit7;
-    if(decode[ENGINE_ON] > 1) return 1;
+    decode[ENGINE_ON] = (code & bit7) / bit7;               // decode ENIGINE_ON bit;
+    if(decode[ENGINE_ON] > 1) return 1;                     // verify ENGINE_ON value;
 
-    decode[GEAR_POS] = (code & (bit6 | bit5 | bit4)) / bit4;
-    if(decode[GEAR_POS] > 4) return 1;
+    decode[GEAR_POS] = (code & (bit6 | bit5 | bit4)) / bit4;// decode GEAR_POS bits;
+    if(decode[GEAR_POS] > 4) return 1;                      // verify GEAR_POS value;
 
-    decode[KEY_POS] = (code & (bit3 | bit2)) / bit2;
-    if(decode[KEY_POS] > 2) return 1;
+    decode[KEY_POS] = (code & (bit3 | bit2)) / bit2;        // decode KEY_POS bits;
+    if(decode[KEY_POS] > 2) return 1;                       // verify KEY_POS value;
 
-    decode[BRAKE1] = (code & bit1) / bit1;
-    if(decode[BRAKE1] > 1) return 1;
+    decode[BRAKE1] = (code & bit1) / bit1;                  // decode BRAKE1 bit;
+    if(decode[BRAKE1] > 1) return 1;                        // verify BRAKE1 value;
 
-    decode[BRAKE2] = code & bit0;
-    if(decode[BRAKE2] > 1) return 1;
-    return 0;
+    decode[BRAKE2] = code & bit0;                           // decode BRAKE2 bit;
+    if(decode[BRAKE2] > 1) return 1;                        // verify BRAKE2 value;
+    return 0;                                               // exit the function;
 }
 
+// Function to print decoded data. Takes code and the decoder function as parameters;
 void printDecode (int code, int (*decoder)(int, char*)) {
-    char decode[] = "00000";
-    if (decoder(code, decode) != 0) {
-        printf("ERROR: faulty input detected");
-        return;
-    } 
+    char decode[] = "00000";                        // initialise decode string;
+    if (decoder(code, decode) != 0) {               // verify decoder output
+        printf("ERROR: faulty input detected");     // print error message;
+        return;                                     // exit the funciton;
+    }
 
-    printf("%-20s%-20s\n", "Name", "Value");
-    printf("%s\n", "-----------------------------");
-    printf("%-20s%d\n", "engine_on", decode[0]);
-    printf("%-20s%d\n", "gear_pos", decode[1]);
-    printf("%-20s%d\n", "key_pos", decode[2]);
-    printf("%-20s%d\n", "brake1", decode[3]);
-    printf("%-20s%d\n", "brake2", decode[4]);
+    printf("%-20s%-20s\n", "Name", "Value");        // print table headers;
+    printf("%s\n", "-----------------------------");// print separator line;
+    printf("%-20s%d\n", "engine_on", decode[0]);    // print engine_on line;
+    printf("%-20s%d\n", "gear_pos", decode[1]);     // print gear_pos line;
+    printf("%-20s%d\n", "key_pos", decode[2]);      // print key_pos line;
+    printf("%-20s%d\n", "brake1", decode[3]);       // print brake1 line;
+    printf("%-20s%d\n", "brake2", decode[4]);       // print brake2 line;
 }
 
 
 int main (int argc, char **argv){
-    if (argc != 2){
-    printf("ERROR: too many or too little arguments.");
-        return 2;
+    if (argc != 2){                                     // check the number of arguments; 
+        printf("ERROR: too many or too little arguments.");// print the error message;
+        return 2;                                       // exit the program with error code;
     }
-    int code = (int) strtol(argv[1], NULL, 16);
+    int code = (int) strtol(argv[1], NULL, 16);         // convert argument to int;
     
-    //for(unsigned char bit = 128; bit > 0; bit >>= 1) printf("%d", (bit&code) && 1);
-    
-    printDecode(code, decoder);
+    if (code == 0 && strcmp(argv[1], "0")) {            // verify the code;
+        printf("ERROR: bad input.");                    // print error message;
+        return 2;                                       // exit the program with error code;
+    }
+
+    printDecode(code, decoder);                         // print decoded data or error;
+    return 0;                                           // exit the program;
 }
